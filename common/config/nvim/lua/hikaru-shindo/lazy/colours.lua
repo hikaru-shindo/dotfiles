4aa25b1c9ca53a77eceb3c8131488e86c98797bb
Pascal Nitsche <saikootau@gmail.com>
2024-06-07 10:31:48 +0200

    Change nvim colour scheme to github_dark_dimmed
    Signed-off-by: Pascal Nitsche <saikootau@gmail.com>

--- common/config/nvim/lua/hikaru-shindo/lazy/colours.lua
+++ common/config/nvim/lua/hikaru-shindo/lazy/colours.lua
@@ -1,6 +1,6 @@
 return {
     "projekt0n/github-nvim-theme", tag = "v1.0.2",
     config = function()
-      vim.cmd.colorscheme("github_dark")
+      vim.cmd.colorscheme("github_dark_dimmed")
     end,
 }
