c79d34c0d0013218459d0751108262650f464427
Pascal Nitsche <saikootau@gmail.com>
2024-07-26 16:05:05 +0200

    Add remap for live_grep in nvim/telescope
    Signed-off-by: Pascal Nitsche <saikootau@gmail.com>

--- common/config/nvim/lua/hikaru-shindo/lazy/telescope.lua
+++ common/config/nvim/lua/hikaru-shindo/lazy/telescope.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-telescope/telescope.nvim",
-    
+
     tag = "0.1.8",
 
     dependencies = {
@@ -12,6 +12,7 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
