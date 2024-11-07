ad7203f4d965526a752e53ef5086cf110ca88370
Pascal Nitsche <saikootau@gmail.com>
2024-11-07 15:39:45 +0100

    Set wezterm font
    Signed-off-by: Pascal Nitsche <saikootau@gmail.com>

--- wezterm/dot-config/wezterm/wezterm.lua
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+config.font_size = 14.0
+config.font = wezterm.font 'JetBrainsMono Nerd Font Mono'
 config.color_scheme = 'Catppuccin Mocha'
 
 return config
