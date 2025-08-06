99128720d0ef7947dc6fa8b477aea5435252b6c3
Pascal Nitsche <saikootau@gmail.com>
2025-08-06 17:08:36 +0200

    Make fzf use catppuccin colours
    Signed-off-by: Pascal Nitsche <saikootau@gmail.com>

--- fish/dot-config/fish/conf.d/env.fish
+++ fish/dot-config/fish/conf.d/env.fish
@@ -18,6 +18,16 @@ else if test -x (command -v vim)
     set -x -g VISUAL vim
 end
 
+if command -sq fzf
+    # Set colours to catppuccin mocha
+    set -x -g FZF_DEFAULT_OPTS "\
+        --color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
+        --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
+        --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
+        --color=selected-bg:#45475A \
+        --color=border:#6C7086,label:#CDD6F4"
+end
+
 if command -sq eza
     alias ls="eza --git --header --icons"
 end
