be063ca16d3870d90d4c4c999b63c89663107bbf
Pascal Nitsche <saikootau@gmail.com>
2025-09-13 11:02:52 +0200

    Fix and update nvim lsp config
    Signed-off-by: Pascal Nitsche <saikootau@gmail.com>

--- nvim/dot-config/nvim/lua/hikaru-shindo/lazy/lsp.lua
+++ nvim/dot-config/nvim/lua/hikaru-shindo/lazy/lsp.lua
@@ -1,9 +1,10 @@
 return {
     "neovim/nvim-lspconfig",
 
-    tag = "v1.7.0",
+    tag = "v2.4.0",
 
     dependencies = {
+        "stevearc/conform.nvim",
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         "hrsh7th/cmp-nvim-lsp",
@@ -17,6 +18,9 @@ return {
     },
 
     config = function()
+        require("conform").setup({
+            formatters_by_ft = {}
+        })
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
@@ -51,16 +55,37 @@ return {
                     }
                 end,
 
+                zls = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.zls.setup({
+                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+                        settings = {
+                            zls = {
+                                enable_inlay_hints = true,
+                                enable_snippets = true,
+                                warn_style = true,
+                            },
+                        },
+                    })
+                    vim.g.zig_fmt_parse_errors = 0
+                    vim.g.zig_fmt_autosave = 0
+
+                end,
                 ["lua_ls"] = function()
                     local lspconfig = require("lspconfig")
                     lspconfig.lua_ls.setup {
                         capabilities = capabilities,
                         settings = {
                             Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
-                                }
+                                format = {
+                                    enable = true,
+                                    -- Put format options here
+                                    -- NOTE: the value should be STRING!!
+                                    defaultConfig = {
+                                        indent_style = "space",
+                                        indent_size = "2",
+                                    }
+                                },
                             }
                         }
                     }
