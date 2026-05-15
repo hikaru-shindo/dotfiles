b657b35aae7efa1b4dcc855d6d86525ee90cf320
Pascal Nitsche <saikootau@gmail.com>
2026-05-15 15:04:32 +0200

    Update lutris-init script for new package locations
    This change also allows the usage of doas instead of sudo, if present
    
    Signed-off-by: Pascal Nitsche <saikootau@gmail.com>

--- init_lutris_arch.sh
+++ init_lutris_arch.sh
@@ -3,11 +3,9 @@
 set -euo pipefail
 
 aur_helper=""
-if [[ $(command -v paru) ]];
-then
+if [[ $(command -v paru) ]]; then
     aur_helper=paru
-elif [[ $(command -v yay) ]];
-then
+elif [[ $(command -v yay) ]]; then
     aur_helper=yay
 fi
 
@@ -16,24 +14,39 @@ if [[ -z "$aur_helper" ]]; then
     exit 1
 fi
 
+id_helper=""
+if [[ $(command -v doas) ]]; then
+    id_helper=doas
+elif [[ $(command -v sudo) ]]; then
+    id_helper=sudo
+fi
+
+if [[ -z "$id_helper" ]]; then
+    echo "Please install and configure doas or sudo to install packages."
+    exit 1
+fi
+
+
+echo "Using ${id_helper} for privileged command execution"
 echo "Using ${aur_helper} for AUR packages"
 
 echo "-> Updating package lists"
-sudo pacman -Sy
-sudo $aur_helper -Sy
+$id_helper pacman -Sy
+$id_helper $aur_helper -Sy
 
 echo "-> Installing wine"
-sudo pacman -S wine-staging winetricks
-sudo pacman -S giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba dosbox
+$id_helper pacman -S wine-staging winetricks
+$id_helper pacman -S giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cups samba dosbox
+$aur_helper -S lib32-gst-plugins-base-libs
 
 echo "-> Installing Steam Runtime"
-sudo pacman -S steam
+$id_helper pacman -S steam
 
 echo "-> Installing protonup-qt"
 $aur_helper -S protonup-qt
 
 echo "-> Installing lutris"
-sudo pacman -S lutris
+$id_helper pacman -S lutris
 
 echo
 echo "Done. Make sure Proton-GE and vlkd3d-proton are available to lutris with protonup-qt!"
